08a7f0dfb27733b02b59926f56d90af7aa23d6ca
brom01 <72760385+brom01@users.noreply.github.com>
2021-04-27 11:03:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vitej ve hře piškvorek");
+            Console.WriteLine("Hrac 1 má o");
+            Console.WriteLine("Hrac 2 má x");
+        }
+    }
+}
